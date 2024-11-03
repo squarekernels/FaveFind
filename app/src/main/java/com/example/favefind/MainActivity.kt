181caafeb478cce96cc8d0c17ccde25e0ac378cf
Hamza Khan <hamzasafdark@gmail.com>
2024-11-03 23:57:30 +0500

    fix text size

--- app/src/main/java/com/example/favefind/MainActivity.kt
+++ app/src/main/java/com/example/favefind/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
